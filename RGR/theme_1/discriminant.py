f6006b02027c1bafb99a0c39b1dbf5916ca08a94
vmuzyk <vadik.big.10@gmail.com>
2025-12-18 01:13:47 +0300

    Delete RGR directory

--- RGR/theme_1/discriminant.py
+++
@@ -1,9 +0,0 @@
-def find_discriminant(a: float, b: float, c: float) -> float:
-   
-    return b**2 - 4*a*c
-
-if __name__ == "__main__":
-    
-    a, b, c = 1, -3, 2
-    d = find_discriminant(a, b, c)
-    print(f"Дискримінант для {a}x² + {b}x + {c} = {d}")
